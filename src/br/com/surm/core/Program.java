84389d92d56b497eafd6503dc958afe4540fa099
Valdigleis <valdigleis@gmail.com>
2025-03-27 16:44:26 -0300

    📄 Atualização texto da licença!

--- src/br/com/surm/core/Program.java
+++ src/br/com/surm/core/Program.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2025 Valdigleis.
+ * Copyright (c) 2025 Valdigleis.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
